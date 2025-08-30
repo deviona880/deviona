9da2b597d68c8a8b81e81a81a9e7d51af89a3e1c
JdidiMedAziz <jdidimedaziz4005@gmail.com>
2025-08-30 17:04:32 +0100

    Update Contact.tsx

--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -30,7 +30,7 @@ const Contact: React.FC = () => {
     setMessageType("")
 
     try {
-      const response = await fetch("http://localhost:5000/contact", {
+      const response = await fetch("https://deviona-backend.onrender.com/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
